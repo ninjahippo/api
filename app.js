60c8c600994ec4099552101fef530193ef1af1a8
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-18 12:16:31 -0500

    changing CORS

--- app.js
+++ app.js
@@ -36,4 +36,10 @@ app.use(function(err, req, res, next) {
     })
 });
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 module.exports = app;
