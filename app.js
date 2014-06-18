56f2fa54c6114be60c5be84e3e8fdfe60c4dce18
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-18 12:19:10 -0500

    apparently i did it wrong?

--- app.js
+++ app.js
@@ -36,10 +36,11 @@ app.use(function(err, req, res, next) {
     })
 });
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
 });
 
 module.exports = app;
