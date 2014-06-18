dac88f341343c4f80a0f8cd347528e41c7df19e7
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-18 11:40:13 -0500

    sometimes two cursors get on my nerves

--- app.js
+++ app.js
@@ -36,12 +36,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.renprocess.env.MONGOLAB_URI || 
-  process.env.MONGOHQ_URL || der('error', {
+    res.render('error', {
         message: err.message,
         error: {}
     });
 });
-
 
 module.exports = app;
