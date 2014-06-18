1a1f9e78b42e43d18726f8828f1083448ef7c800
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-18 12:29:59 -0500

    this is getting a little annoying

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var cors = require('cors')
 
 var routes = require('./routes/index');
 var v1 = require('./routes/v1');
@@ -7,6 +8,8 @@ var v1 = require('./routes/v1');
 var app = express();
 
 mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/ninjahippo');
+
+app.use(cors());
 
 app.use('/', routes);
 app.use('/v1', v1);
@@ -34,13 +37,6 @@ app.use(function(err, req, res, next) {
         message: err.message,
         error: err
     })
-});
-
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    next();
 });
 
 module.exports = app;
