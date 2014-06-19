2177a77641d837fc600cecc239b99f56e9675238
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-18 22:08:08 -0500

    adding crypto back to the dependencies

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.redirect( "http://cms.ninjahippo.io/");
+  res.redirect("http://cms.ninjahippo.io/");
 });
 
 module.exports = router;
